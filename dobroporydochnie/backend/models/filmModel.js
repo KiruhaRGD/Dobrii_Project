3854c89613cd59de6c0078f7be1f0116f15a85e0
bob <nikitazamorov0@gmail.com>
2025-10-03 12:46:02 +0500

    'создание_функций_поиск_добавление_удаление_и_тд'

--- dobroporydochnie/backend/models/filmModel.js
+++ dobroporydochnie/backend/models/filmModel.js
@@ -1,16 +1,37 @@
 const db = require('../config/database');
 
-async function getAllFilms() {
-  const result = await db.query('SELECT * FROM films');
-  return result.rows;
-}
 
-async function addFilm(title, year, genre) {
-  const result = await db.query(
-    'INSERT INTO films (title, year, genre) VALUES ($1, $2, $3) RETURNING *',
-    [title, year, genre]
-  );
-  return result.rows[0];
-}
+  async function getAllFilms() {
+   const result = await db.query('SELECT * FROM film');
+    return result.rows;
+  }
 
-module.exports = { getAllFilms, addFilm };
+  async function addFilm(title, year, genre) {
+    const result = await db.query(
+      'INSERT INTO film (title, year, genre) VALUES ($1, $2, $3) RETURNING *',
+      [title, year, genre]
+    );
+    return result.rows[0];
+  }
+
+  async function deletefilms(id) {
+    const result = await db.query('DELETE FROM film WHERE id = $1;', 
+      [id]
+    );
+    return result.rows[0];
+  }
+
+  async function updatefilm(title, year, genre) {
+    const result = await db.query('update film set (titlem, year, genre) = ($1, $2, $3);', 
+      [title, year, genre]
+    );
+    return result.rows[0];
+  }
+
+  async function getFilm(id) {
+    const result = await db.query('SELECT * FROM id = $1',
+      [id]
+    );
+    return result.rows;
+  }
+module.exports = { getAllFilms, addFilm, deletefilms, updatefilm, getFilm };
