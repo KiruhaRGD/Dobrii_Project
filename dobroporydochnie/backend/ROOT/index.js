c4a811a08321ca3603fd75d55ad8ef71db0265ff
bob <nikitazamorov0@gmail.com>
2025-10-20 12:29:26 +0500

    add_user

--- dobroporydochnie/backend/ROOT/index.js
+++ dobroporydochnie/backend/ROOT/index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 const filmsRouter = require('../routes/films');
-
+const userRouter = require('../routes/user');
 
 app.use(express.json());
 app.use('/films', filmsRouter);
-
+app.use('/user', userRouter);
 const errorHandler = require('../middlewares/errorHandler');
 app.use(errorHandler);
 
