eb4597d64b98b5abe49012da64becd12e49d6820
KiruhaRGD <kirilllukinskih@gmail.com>
2025-10-31 04:55:13 +0500

    3
    dark theme ying-yang, bububebe

--- project_/src/App.jsx
+++ project_/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import kinologo from './kino.png'
 import zakl from './zakl.png'
 import p from './p.png'
 import found from './found.png'
+import Logo from './Logo.png'
+import InYan from './inyan.png'
+import stena from './stena.png'
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    document.body.className = darkMode ? "dark-theme" : "light-theme";
+  },[darkMode]);
 
   return (
     <>
       
         <header>
         <div className='Container'>
-          <button className='MainButton'>电影 ЧинаПоиск |</button>
-       
-           <img src={kinologo} alt='image sdox' className='kino'/>
-          <button className='MainButton'>Подбор фильма |</button>  
-            <img src={zakl} alt='image sdox' className='kino'/>
-             <button className='MainButton'>Закладки</button> 
-            <input type="" id="" name="" className='input1'/>
-             <img src={p} alt='image sdox' className='kino'/>
-            <button className='MainButton'>Вход</button> 
-             <img src={found} alt='image sdox' className='kino2'/>
+
+          <button className='Logo'> <img src={Logo} alt='image sdox' className='Logo-img'/>ЧинаПоиск </button>
+          <img src={stena} alt='image sdox' className='stena'/>
+          <button className='MainButton'> <img src={kinologo} alt='image sdox' className='kino'/>Подбор фильма </button>  
+          <img src={stena} alt='image sdox' className='stena'/>
+          <button className='MainButton'> <img src={zakl} alt='image sdox' className='kino'/>Закладки</button> 
+          <div className='search-container'>
+          <input type="text" placeholder="Поиск фильмов..." className='search-input' />
+            <button className='search-button'>
+              <img src={found} alt="Поиск" className='search-icon' />
+            </button>
           </div>
-      </header>
+          <button className='MainButton'><img src={p} alt='image sdox' className='kino'/>Вход</button> 
+          <button className={`theme-toggle ${darkMode ? 'rotated' : ''}`}onClick={() => setDarkMode(!darkMode)}>
+          <img src={InYan} alt="image sdox" className="InYan" />
+          </button>       
+        </div>
+        </header>
+
       <body>
         <div className='Container2'>
              <div className='Container3'>
-                <div className='Container4'></div>
+                <div className='Container4'>
+                  
+                </div>
              </div>
         </div>
       </body>
