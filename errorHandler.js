24ce7a04631a4ddc10ee0a36802c69bea52d9ce2
KiruhaRGD <kirilllukinskih@gmail.com>
2025-10-31 00:25:21 +0500

    Delete errorHandler.js

--- errorHandler.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(req,res,next){
-  const time = new Date().toLocaleTimeString("ru-RU",{hour: '2-digit', minute: '2-digit'});
-  console.log(req.method, req.url, time);
-  next()
-}
-
-module.exports = errorHandler;
