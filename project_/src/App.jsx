a424306f148e32e2e7e9ca605a7d02864da211e7
KiruhaRGD <kirilllukinskih@gmail.com>
2025-10-31 06:42:30 +0500

    6
    ВАУ Я ИДУ НА ПАРУ

--- project_/src/App.jsx
+++ project_/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
 import kinologo from './kino.png'
 import zakl from './zakl.png'
 import p from './p.png'
@@ -20,6 +22,7 @@ function App() {
     document.body.className = darkMode ? "dark-theme" : "light-theme";
   },[darkMode]);
 
+    
   return (
     <>
       
@@ -57,6 +60,31 @@ function App() {
                 <div className='Container5'>
                   <div className='novinkiText'>Новинки кино!</div>
                   <div className='sliderNovinki'></div>
+                  <Carousel>
+                    <Carousel.Item>
+                      <ExampleCarouselImage text="First slide" />
+                      <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <ExampleCarouselImage text="Second slide" />
+                      <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <ExampleCarouselImage text="Third slide" />
+                      <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
 
                 </div>
              </div>
