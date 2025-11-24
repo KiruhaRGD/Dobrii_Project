c9ff7c40335e673ef0dd6a36d9d8bb57b7714ff9
KiruhaRGD <kirilllukinskih@gmail.com>
2025-11-24 13:49:37 +0500

    working project

--- project_/src/App.jsx
+++ project_/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
 import kinologo from './kino.png'
 import zakl from './zakl.png'
 import p from './p.png'
@@ -60,31 +58,7 @@ function App() {
                 <div className='Container5'>
                   <div className='novinkiText'>Новинки кино!</div>
                   <div className='sliderNovinki'></div>
-                  <Carousel>
-                    <Carousel.Item>
-                      <ExampleCarouselImage text="First slide" />
-                      <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <ExampleCarouselImage text="Second slide" />
-                      <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <ExampleCarouselImage text="Third slide" />
-                      <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                  </Carousel>
+        
 
                 </div>
              </div>
